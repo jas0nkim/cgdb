e7430607068247cbf422116862ab5602034e8874
Jason Kim <jason.kim.jiho@gmail.com>
2020-10-21 15:33:01 -0400

    updated README, django db connection

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ Provides easily searchable game titles by cloud gaming platforms, such as micros
 - Search / display information oriented site
 - Search by game / platform / country
 - Multi-language support
+
+## Installation
+
+1. Run containers
+```
+$ docker-compose up -d
+```
+
+2. Initialize django database and create a superuser
+```
+$ docker-compose exec backend python manage.py migrate
+$ docker-compose exec backend python manage.py createsuperuser
+```
--- backend/src/cgdb/settings.py
+++ backend/src/cgdb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'cgdb.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASS'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['DATABASE_PORT'],
     }
 }
 
