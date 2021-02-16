a8c42c73e2d3800dc84126ba7199998fc1ea6028
Jason Kim <jason.kim.jiho@gmail.com>
2021-02-16 14:24:31 -0500

    frontend - removed HideOnScroll on MenuBar

--- frontend/src/components/MenuBar.js
+++ frontend/src/components/MenuBar.js
@@ -12,29 +12,6 @@ import { AppBar, IconButton, Toolbar, List, ListItem, ListItemText, Divider }
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchBox from "./SearchBox"
 
-function HideOnScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({ target: window ? window() : undefined });
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-HideOnScroll.propTypes = {
-  children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
-
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -111,7 +88,7 @@ const MenuBar = (props) => {
   );
 
   return (
-    <HideOnScroll {...props}>
+    <>
       <div className={classes.root}>
         <AppBar position="fixed" className={classes.appBar}>
           <Toolbar>
@@ -145,7 +122,7 @@ const MenuBar = (props) => {
             </Drawer>
         </nav>
       </div>
-    </HideOnScroll>
+    </>
   );
 }
 
